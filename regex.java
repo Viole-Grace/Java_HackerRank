33d4fbbfafda3fe097e8e889ca889a5c959f1028
Aamir Syed <34801512+Viole-Grace@users.noreply.github.com>
2018-06-14 22:24:32 +0530

    Create regex.java

---
+++ regex.java
@@ -0,0 +1,5 @@
+//Write your code here
+class MyRegex {
+    String from0To255 = "(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])";
+    public String pattern = String.format("%s.%s.%s.%s", from0To255, from0To255, from0To255, from0To255);
+}
