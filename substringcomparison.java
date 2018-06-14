05dbedece97f93d384b9b1e617ff1df96c9c39c3
Aamir Syed <34801512+Viole-Grace@users.noreply.github.com>
2018-06-14 22:20:34 +0530

    Create substringcomparison.java

---
+++ substringcomparison.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        int i=0;
+        SortedSet<String> sets=new TreeSet<String>();
+        for(i=0;i<=s.length()-k;i++)
+        {
+            sets.add(s.substring(i,i+k));
+        }
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        System.out.println(sets.first());
+        System.out.println(sets.last());
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
