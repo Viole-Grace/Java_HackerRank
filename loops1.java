5113b64cbf35273c844a4e0b710276f2f5a43a49
Aamir Syed <34801512+Viole-Grace@users.noreply.github.com>
2018-06-14 22:14:16 +0530

    Create loops1.java

---
+++ loops1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        for(int c=1;c<=10;c++)
+        {
+            System.out.println(i+" x "+c+" = "+i*c);
+        }
+    }
+}
