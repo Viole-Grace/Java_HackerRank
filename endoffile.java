78252b2f50a3bfb596d65fc2c587ad6d5cbc7e98
Aamir Syed <34801512+Viole-Grace@users.noreply.github.com>
2018-06-14 22:15:54 +0530

    Create endoffile.java

---
+++ endoffile.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner userInput = new Scanner(System.in);
+        int lineNum = 1;    
+    
+        while (userInput.hasNextLine() == true) 
+        {
+            if (userInput.hasNext() == true) 
+            {
+                System.out.println(lineNum + " " + userInput.nextLine());
+                lineNum++;
+            }
+        }/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
