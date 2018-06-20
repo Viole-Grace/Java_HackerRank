c8f64442825f0f2287967aa4e3e12040ed1d0e6a
Aamir Syed <34801512+Viole-Grace@users.noreply.github.com>
2018-06-21 00:30:04 +0530

    Create interface.java

---
+++ interface.java
@@ -0,0 +1,13 @@
+//Write your code here
+class MyCalculator implements AdvancedArithmetic {
+    int j = 0;
+   public int divisor_sum(int n) {
+        for(int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                j = j + i; 
+            }
+        }
+        return j;
+    }
+   
+}
